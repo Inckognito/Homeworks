d8b2f298f331569e8e34508e158b40d17897b6b9
Inckognito <evgeni_ivanovv1@abv.bg>
2017-09-27 21:36:30 +0300

    Homeworks

---
+++ Programming Fundamentals2/03.MilestoKilometers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.MilestoKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometers = double.Parse(Console.ReadLine());
+            double miles = kilometers * 1.60934;
+
+            Console.WriteLine($"{miles:f2}");
+        }
+    }
+}
