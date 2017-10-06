c3ddaf87f23cb7ce2e7e73b55ad47f9539537f47
Inckognito <evgeni_ivanovv1@abv.bg>
2017-10-06 22:48:15 +0300

    Renamed all folders, and added Methods,Debugging and Troubleshooting - Excersises

---
+++ Methods. Debugging and Troubleshooting Code - Exercises/05.FibonacciNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number = uint.Parse(Console.ReadLine());
+            Console.WriteLine(FibonachiNum(number));
+        }
+
+        static UInt64 FibonachiNum (uint number)
+        {
+            uint a = 0;
+            uint b = 1;
+            uint c = 1;
+
+            for (uint i = 0; i < number; i++)
+            {
+                c = b + a;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+}
