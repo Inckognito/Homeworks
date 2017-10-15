6bcd38837aaa57a93d9bdbe5e00e1fe974a38a26
Inckognito <evgeni_ivanovv1@abv.bg>
2017-10-15 22:02:33 +0300

    added dictionaries and LINQ excersises

---
+++ Dictionaries,LambdaAndLINQ-Exercises/04.FixEmails/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canContinue = true;
+            Dictionary<string, string> eMails = new Dictionary<string, string>();
+
+            while (canContinue)
+            {
+                string name = Console.ReadLine();
+                string emailAdress = "";
+                if (name == "stop")
+                {
+                    canContinue = false;
+                    break;
+                }
+                else
+                {
+                    emailAdress = Console.ReadLine();
+                }
+
+                bool isUsUkEmail = false;
+                string mailTL = emailAdress.ToLower();
+
+                if (emailAdress.Contains(".us") || emailAdress.Contains(".uk"))
+                {
+                    isUsUkEmail = true;
+                }
+
+                if (isUsUkEmail == false)
+                {
+                    if (eMails.ContainsKey(name))
+                    {
+                        eMails[name] = emailAdress;
+                    }
+                    else
+                    {
+                        eMails.Add(name, emailAdress);
+                    }
+                }
+            }
+            foreach (KeyValuePair<string, string> entry in eMails)
+            {
+                Console.WriteLine($"{entry.Key} -> {entry.Value}");
+            }
+        }
+    }
+}
