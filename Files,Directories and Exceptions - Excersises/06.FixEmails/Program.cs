a0da4b53c417219ee505c42e11861a97d275c01f
Inckognito <evgeni_ivanovv1@abv.bg>
2017-10-23 23:11:20 +0300

    added Files,Directories and Exceptions - Excersises

---
+++ Files,Directories and Exceptions - Excersises/06.FixEmails/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            for (int i = 0; i < lines.Length; i += 2)
+            {
+                if (lines[i] == "stop" || lines[i + 1] == "stop")
+                {
+                    break;
+                }
+                string name = lines[i];
+                string email = lines[i + 1];
+
+                if (email.EndsWith(".uk") || email.EndsWith(".us"))
+                {
+                    continue;
+                }
+                string result = $"{name} -> {email}";
+                File.AppendAllText("output.txt", result + Environment.NewLine);
+            }
+
+        }
+    }
+}
